a5cac961102c2650dc330eefa9e070127c8a2316
nardo <129193276+nardomaciel@users.noreply.github.com>
2023-10-13 18:03:48 -0300

    break

--- aula018/sorteio.php
+++ aula018/sorteio.php
@@ -5,22 +5,31 @@ echo $nsort;
 switch($nsort){
     case 17:
         echo "joao ganhou";
+        break;
     case 67:
         echo "leonardo ganhou";
+        break;
     case 21:
         echo "esdras ganhou";
+        break;
     case 33:
         echo "igor ganhou";
+        break;
     case 12:
         echo "manu ganhou";
+        break;
     case 7:
         echo "vini ganhou";
+        break
     case 4:
-         echo "maculan ganhou";
+         echo "Maculan ganhou";
+         break;
     case 69:
           echo "Thales ganhou";
+          break;
     case  6:
-        echo "caua ganhou";    
+        echo "caua ganhou";
+        break;    
     case 75:
          echo "zimmermann ganhou";
          break;
