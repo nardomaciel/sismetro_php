cfaf4002d4d0f31f7e5efd4bb45760fe3bf873fd
nardo <129193276+nardomaciel@users.noreply.github.com>
2023-10-27 08:45:58 -0300

    tentativa 01

--- aula20/exercicio_02.php
+++ aula20/exercicio_02.php
@@ -1,9 +1,23 @@
 <?php
-function primo($pr){
-    $primos = array();
-    $primos[0] = 1;
-    $primos[1] = 2;
-    $primos[2] = 3;
-   while(count($primos) < $pr)
+function isPrimo($num) {
+    // Verifica se o número é menor que 2, pois números menores que 2 não são primos
+    $num = array();
+    $num[0]= 1;
+    $num[1]= 2; 
+    if ($num < 2) {
+        return false;
+    }
+
+    // Percorre todos os números de 2 até a metade do número e verifica se há algum divisor
+    for ($i = 2; $i <= $num[0]; $i++) {
+        if ($num%$i == 0) {
+            return false;
+        }
+    }
+
+    // Se nenhum divisor foi encontrado, o número é primo
+    return true;
+    
 }
-echo primos
+echo isPrimo(10);
+?>
